c2da86f53f11dc107e6f5a5e94918a6a7f1c83af
suryotriatmojo <atmojo.suryo@gmail.com>
2019-07-02 20:13:24 +0700

    first

---
+++ README.md
@@ -0,0 +1,31 @@
+# DUMMY APP OF TITANIC PASSENGER SURVIVAL
+
+#
+
+Using __Logistic Regression__ method to predict the passenger survival in Titanic accident.
+
+At the home page you are supposed to give input on one passenger through several criterias:
+
+![kapal_titanic](images/home.png)
+
+Then, the machine will predict whether that passenger is managed to be saved or dead.
+
+- If the passenger is save will show:
+![save](images/save.png)
+
+- Else the passenger is predicted to be dead and will show:
+![dead](images/dead.png)
+
+#
+#### About Me: Suryo Tri Atmojo 👨‍🔬👨‍💻
+#### Reach me out 📬 : _atmojo.suryo@gmail.com_
+
+[GitHub](https://github.com/suryotriatmojo)
+|
+[Instagram](https://www.instagram.com/suryotriatmojo/)
+|
+[Facebook](https://www.facebook.com/suryo.t.atmojo)
+|
+[LinkedIn](https://www.linkedin.com/in/suryo-tri-atmojo-3ab69a85/)
+|
+[Twitter](https://twitter.com/suryota)
---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        
+        sex = int(request.form['sex'])
+        if sex == 0:
+            female = 1
+            male = 0
+        else:
+            female = 0
+            male = 1
+
+        age = int(request.form['age'])
+        if sex == 0 and age >= 18:
+            child = 0
+            man = 0
+            woman = 1
+            adultM = 0
+        if sex == 1 and age >= 18:
+            child = 0
+            man = 1
+            adultM = 1
+            woman = 0
+        else:
+            child = 1
+            man = 0
+            woman = 0
+            adultM = 0
+
+        pclass = int(request.form['class'])
+        sibsp = int(request.form['sibsp'])
+        parch = int(request.form['parch'])
+
+        if sibsp == 0 or parch == 0:
+            alone = 1
+        else:
+            alone = 0
+
+        fare = int(request.form['fare'])
+
+        fitur = [female, male, child, man, woman, pclass, age, sibsp, parch, fare, adultM, alone]
+        print(fitur)
+        prediksi = model.predict([fitur])[0]
+        
+        return redirect(url_for('show_prediction', prediksi = prediksi))
+    else:        
+        return render_template('home.html')
+
+@app.route('/result/<int:prediksi>')
+def show_prediction(prediksi):
+    print(prediksi)
+    if prediksi == 1:
+        # hasil = 'Orang yang Anda cari selamat'
+        return render_template('selamat.html')
+    else:
+        return render_template('mati.html')
+
+if __name__ == '__main__':
+    model = joblib.load('model_titanic')
+    app.run(debug = True)
